507529e66e00d4cb7d4bd5e41c324ef67e9bb712
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-02 17:02:16 +0100

    Day 2 done

--- 2/2.py
+++ 2/2.py
@@ -17,12 +17,14 @@ class TestWft(unittest.TestCase):
 
     def test_part_1(self):
         sum = 0
+        power_sum = 0
         with open("input_2.txt", mode='r', encoding='utf-8') as f:
             while (line := f.readline()):
                 parts = line.split(':', maxsplit=1)
                 game_no = int(parts[0].split(' ', maxsplit=1)[1])
                 print(f"{game_no}: {line}")
                 max = {}
+
                 max['blue'] = 0
                 max['green'] = 0
                 max['red'] = 0
@@ -41,12 +43,18 @@ class TestWft(unittest.TestCase):
 
                         if no_of_cubes > max[cube_col]:
                             max[cube_col] = no_of_cubes
+
+
                 print(f"max: {max}")
 
 
                 if  max['blue'] <= 14 and max['red'] <= 12 and max['green'] <= 13:
                     sum += game_no
+                power_sum +=  max['blue'] * max['red'] * max['green']
+
         print(f"Day 2, part1: {sum}")
+        print(f"Day 2, part2: {power_sum}")
+
 
 if __name__ == '__main__':
     unittest.main()
