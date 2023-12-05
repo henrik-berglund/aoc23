e32e7744648e6da2c4b875bcf480773302db15ea
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-05 17:27:55 +0100

    .

---
+++ 6/6.py
@@ -0,0 +1,36 @@
+import unittest
+from approvaltests.approvals import verify
+from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
+import re
+
+class TestAoc(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        factory = GenericDiffReporterFactory()
+        factory.load('myreporters.json')
+        self.reporter = factory.get_first_working()
+        self.char_encoding = 'iso-8859-1'
+        pass
+
+    def readfile(self, file_name):
+        lines = []
+        with open(file_name, mode='r', encoding='utf-8') as f:
+            while (line := f.readline()):
+                lines.append(line)
+        return lines
+
+    def test_part_1(self):
+        part1_res = 0
+        file = self.readfile("input_6.txt")
+        for line in file:
+            pass
+
+        print(f"Day 3, part1: {part1_res}")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
