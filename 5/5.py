bdba834cd0ce5fa1899bee2c7cf00d4f50ea765a
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-05 06:47:15 +0100

    .

--- 5/5.py
+++ 5/5.py
@@ -7,17 +7,27 @@ headings = ['seeds',
             'temperature-to-humidity',
             'humidity-to-location map' ]
 
+def mode_line(line):
+    mode = None
+    for h in headings:
+        if h in line:
+            mode = h
+    return mode
+
 def part1():
     with open("input_5.txt", mode='r', encoding='utf-8') as f:
         while (line := f.readline()):
             if 'seeds' in line:
                 nums = line[7:].split(' ')
-                seeds = [num.strip() for num in nums]
+                seeds = [int(num.strip()) for num in nums]
                 print(seeds)
-            else:
-                for h in headings:
-                    if h in line:
-                        print(line)
+            elif (mode := mode_line(line)):
+                print("mode ", mode)
+            elif len(line.strip()) != 0:
+                tuple = line.split(' ')
+                tuple = [int(num.strip()) for num in tuple]
+                print(tuple)
+
 
 
 part1()
