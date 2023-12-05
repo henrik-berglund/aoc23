c34cd8bdb0f86ee6be369f4774aa8224fd985bb0
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-05 06:54:12 +0100

    .

--- 5/5.py
+++ 5/5.py
@@ -1,11 +1,10 @@
-headings = ['seeds',
-            'seed-to-soil',
+headings = ['seed-to-soil',
             'soil-to-fertilizer',
             'fertilizer-to-water',
             'water-to-light',
             'light-to-temperature',
             'temperature-to-humidity',
-            'humidity-to-location map' ]
+            'humidity-to-location' ]
 
 def mode_line(line):
     mode = None
@@ -15,18 +14,24 @@ def mode_line(line):
     return mode
 
 def part1():
+    maps = {}
+
     with open("input_5.txt", mode='r', encoding='utf-8') as f:
         while (line := f.readline()):
             if 'seeds' in line:
                 nums = line[7:].split(' ')
                 seeds = [int(num.strip()) for num in nums]
                 print(seeds)
-            elif (mode := mode_line(line)):
-                print("mode ", mode)
+            elif (match := mode_line(line)):
+                mode = match
+                #print("mode: ", mode)
+                maps[mode] = []
             elif len(line.strip()) != 0:
                 tuple = line.split(' ')
                 tuple = [int(num.strip()) for num in tuple]
-                print(tuple)
+                #print(tuple, mode)
+                maps[mode].append(tuple)
+    print(maps)
 
 
 
