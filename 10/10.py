757842c16e2a1ed14a32bd24bc85e936e4afacb9
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-10 18:22:12 +0100

    .

--- 10/10.py
+++ 10/10.py
@@ -31,11 +31,16 @@ def part1():
     visited_nodes[key(s_x, s_y)] = 0
     new_to_visit.append((s_x, s_y))
 
+    connects_to_left = ['-', 'J', '7', 'S']
+    connects_to_right = ['-', 'L', 'F', 'S' ]
+    connects_up = ['|', 'L', 'J', 'S' ]
+    connects_down = ['|', 'F', '7', 'S']
+
     moves = [
-        ((-1,0), ['-', 'L', 'F' ]),
-        ((1,0),  ['-', 'J', '7']),
-        ((0,-1), ['|', 'F', '7']),
-        ((0,1),  ['|', 'L', 'J' ] )]
+        ((-1,0), connects_to_right, connects_to_left),
+        ((1,0),  connects_to_left, connects_to_right),
+        ((0,-1), connects_down, connects_up),
+        ((0,1),   connects_up, connects_down)]
 
     dist=-1
     max_dist=0
@@ -46,11 +51,12 @@ def part1():
         while len(to_visit)> 0:
             node = to_visit.pop(0)
             x, y = node
+            c_this_node = grid[y][x]
             visited_nodes[key(x, y)] = dist
 
             #print(f"dist {dist} checking {x},{y}")
             for m in moves:
-                offset, valid_connects = m
+                offset, valid_connects, outgoing_valid_connects = m
                 new_x = x + offset[0]
                 new_y = y + offset[1]
 
@@ -58,34 +64,20 @@ def part1():
 
                 if new_x >= 0 and new_y >= 0 and new_x < len(grid[0]) and new_y < len(grid):
                     c = grid[new_y][new_x]
-                    if c in valid_connects:
+                    if c in valid_connects and c_this_node in outgoing_valid_connects:
                         if not key(new_x, new_y) in visited_nodes:
                             new_to_visit.append((new_x,new_y))
                             max_dist = dist + 1
 
-                        #print(f"---added {new_x},{new_y}")
+                        #print(f"---added {new_x}-{new_y} from {x},{y}")
 
-    for y, line in enumerate(grid):
-        for x, c in enumerate(line):
-            if key(x,y) in visited_nodes:
-                print(visited_nodes[key(x,y)], " ", end="")
-            else:
-                print(". ", end="")
-        print()
 
     print("Max dist ", max_dist)
 
 
-    for y, line in enumerate(grid):
-        for x, c in enumerate(line):
-            if key(x,y) in visited_nodes:
-                print(" X ", end="")
-            else:
-                c = count(x, y, visited_nodes, grid)
-                print(f"{c:3}", end="")
-        print()
-
     plot(grid, visited_nodes)
+
+
 
 def count(x, y, visited, grid):
 
@@ -112,7 +104,6 @@ def key(x,y):
     return str(x) + "-" + str(y)
 
 
-
 def plot(grid, visited):
     # Define the range of x and y values to check
     x_range = range(0, len(grid[0]))
@@ -122,28 +113,40 @@ def plot(grid, visited):
     loop_points_x = []
     loop_points_y = []
     non_loop_points_x = []
-    non_loop_points_y = []
+    tiles_x = []
+    tiles_y = []
+
+    garbage_x = []
+    garbage_y = []
 
     # Check each point in the grid
+
     for x in x_range:
         for y in y_range:
             if key(x,y) in visited:
                 loop_points_x.append(x)
                 loop_points_y.append(y)
             else:
-                non_loop_points_x.append(x)
-                non_loop_points_y.append(y)
+                tiles_x.append(x)
+                tiles_y.append(y)
+
+    k = "10-4"
+    print(k, k in visited )
 
     # Plotting
     plt.scatter(loop_points_x, loop_points_y, color='blue', label='Loop Points')  # Loop points in blue
-    plt.scatter(non_loop_points_x, non_loop_points_y, color='red', label='Non-loop Points')  # Non-loop points in red
+    plt.scatter(tiles_x, tiles_y, color='red', label='Non-loop Points')  # Non-loop points in red
+    plt.scatter(garbage_x, garbage_y, color='black', label='Non-loop Points')  # Non-loop points in red
+
     plt.xlabel('X-axis')
     plt.ylabel('Y-axis')
     plt.title('Visualization of the Loop Structure')
-    plt.legend()
-    plt.grid(False)
-    plt.show()
+    plt.grid(False)  # Disables the grid
 
+    # Inverting the y-axis
+    plt.gca().invert_yaxis()
+
+    plt.show()
 
 
 
