c1c710d8f7a3a27c97fc83a15c28c9fbd8f8020c
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-10 08:06:34 +0100

    .

--- 10/10.py
+++ 10/10.py
@@ -39,43 +39,23 @@ def part1():
 #. is ground; there is no pipe in this tile.
 def add_neighbors(grid, to_visit, visited_nodes, x, y, dist):
 
-    new_dist = dist+1
-    if x > 0:
-        new_x = x-1
-        new_y = y
+    moves = [
+        ((-1,0), ['-', 'L', 'F' ]),
+        ((1,0),  ['-', 'J', '7']),
+        ((0,-1), ['|', 'F', '7']),
+        ((0,1),  ['|', 'L', 'J' ] )]
 
-        c = grid[new_y][new_y]
-        if c in ['-', 'L', 'F' ]:
+    for m in moves:
+        offset, chars = m
+        new_x = x + offset[0]
+        new_y = y + offset[1]
+
+        if new_x > 0 and new_y > 0 and new_x < len(grid[0]) and new_y < len(grid):
+            new_dist = dist+1
             if not key(new_x, new_y) in visited_nodes:
                 to_visit.append((new_x,new_y, new_dist))
 
-    if x+1 <  len(grid[0]):
-        new_x = x+1
-        new_y = y
-
-        c = grid[new_y][new_y]
-        if c in ['-', 'J', '7' ]:
-            if not key(new_x, new_y) in visited_nodes:
-                to_visit.append((new_x,new_y, new_dist))
-
-    if y > 0:
-        new_x = x
-        new_y = y-1
-
-        c = grid[new_y][new_y]
-        if c in ['|', 'F', '7' ]:
-            if not key(new_x, new_y) in visited_nodes:
-                to_visit.append((new_x,new_y, new_dist))
-
-    if y+1 < len(grid):
-        new_x = x
-        new_y = y+1
-
-        c = grid[new_y][new_y]
-        if c in ['|', 'L', 'J' ]:
-            if not key(new_x, new_y) in visited_nodes:
-                to_visit.append((new_x,new_y, new_dist))
-
+    
 def key(x,y):
     return str(x) + "-" + str(y)
 
