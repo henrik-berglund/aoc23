cf9ce53d49b4bbba8592b51da72d645ccd4dd8fd
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-11 07:19:34 +0100

    .

--- 11/11.py
+++ 11/11.py
@@ -41,8 +41,43 @@ def part1():
             if c == '#':
                 coords.append((x,y))
 
+    sum = 0
+    for i, c1 in enumerate(coords):
+        for c2 in coords[i+1:]:
+            xd = abs(c1[0]-c2[0])
+            yd = abs(c1[1]-c2[1])
+            dist = xd+yd
+            sum += dist
+            print(f"From {c1} to {c2} = {xd} {yd}")
+
+    print("Part 1", sum)
+
+def part2():
+    lines = readfile(11)
+
+    coords = []
+    for y, l in enumerate(lines):
+        for x, c in enumerate(l):
+            if c == '#':
+                coords.append((x,y))
+
+    print(coords)
+
+    y_add = []
+    for y, l in enumerate(lines):
+        if not '#' in l:
+            y_add.append(y)
+    print(y_add)
+
+    c2 = []
     for c in coords:
-        print(c)
+        x,y = c
+        a = 0
+        for ya in y_add:
+            if y > ya:
+                a+= 1
+        c2.append((x, y+a))
 
+    print(c2)
 
-part1()
+part2()
