9951537543ae4fa47b2a72deba89b3b1947cd7c2
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-11 06:55:26 +0100

    .

---
+++ 11/11.py
@@ -0,0 +1,41 @@
+def readfile( day):
+    file_name = f"input_{day}.txt"
+    lines = []
+    with open(file_name, mode='r', encoding='utf-8') as f:
+        while (line := f.readline()):
+            lines.append(line.strip())
+    return lines
+
+def part1():
+    lines = readfile(11)
+
+    lines2 = []
+    while len(lines) > 0:
+        line = lines.pop(0)
+        lines2.append(line)
+        if not '#' in line:
+            lines2.append(line)
+
+    extend = []
+    for col in range(len(lines2[0])):
+        found = False
+        for l in lines2:
+            if l[col] == '#':
+                found = True
+        if not found:
+            extend.append(col)
+
+    extend.sort(reverse=True)
+    print(extend)
+
+    lines = []
+    for l in lines2:
+        ll = list(l)
+        for c in extend:
+            ll.insert(c,'.')
+        lines.append(ll)
+
+    for l in lines:
+        print(l)
+
+part1()
