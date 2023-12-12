dfd32ebf2b4ec6c01ae43163ed3237c68019ebdf
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-12 06:39:57 +0100

    .

--- 12/12.py
+++ 12/12.py
@@ -15,8 +15,30 @@ def part1():
         counts = [int(n) for n in c]
         parts.append((p,counts))
 
+    cparts = []
     for p in parts:
-        print(p)
+        line = []
+        dcount =0
+        for c in p[0]:
+            if c == '#':
+                dcount += 1
+            elif dcount != 0:
+                line.append(('d', dcount))
+                dcount=0
+
+                line.append(c)
+            else:
+                line.append(c)
+
+        if dcount != 0:
+            line.append(('d', dcount))
+            dcount = 0
+
+        cparts.append(line)
+
+
+        for i, p in enumerate(cparts):
+            print(f"{lines[i]}\n***{p}\n")
 
 
 part1()
