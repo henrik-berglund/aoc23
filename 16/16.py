4350a37debe14e2a45cf9535ccda5914c96df602
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-17 12:27:22 +0100

    .

--- 16/16.py
+++ 16/16.py
@@ -41,7 +41,7 @@ def part2():
     dx = 1
     dy = 0
     previous_paths = []
-    for y in range(4):
+    for y in range(10):
         ray = (x, y, dx, dy)
         path = []
         energized = set()
@@ -87,24 +87,6 @@ def part2():
 
     print("16.2: ", max_count)
 
-def dumpener(grid, count, ener):
-
-    for s in ener:
-        st = s.split(",")
-        x = int(st[0])
-        y = int(st[1])
-
-    if count %100 != 0:
-        return
-    #print("len: ", len(ener))
-    return
-    for y, l in enumerate(grid):
-        for x, d in enumerate(l):
-            if f"{x},{y}" in ener:
-                print("#", end="")
-            else:
-                print(".", end = "")
-        print()
 
 def count_energized(count, grid, ener, ray, ray_path, previous_paths):
     width = len(grid[0])
@@ -141,49 +123,27 @@ def count_energized(count, grid, ener, ray, ray_path, previous_paths):
         ray_path.append(ray)
 
         ener.add((x,y))
-        #dumpener(grid, count, ener)
 
         c = grid[y][x]
-        #print(f"rays {rays} ")
-        ltype = '- pop' if dopop else '-'
-        #print(f"- {ltype} {ray} meets {c}", flush=True, end="")
         count += 1
 
         if c == '.' or (c == '|' and dy != 0 ) or (c == '-' and dx != 0 ):
-
             x += dx
             y += dy
-            #count_energized(count, grid,ener,   )
             ray1 = (x, y, dx, dy)
-
-            #print("=> ray ", ray1)
-
         elif c == '|':
-
             ray1 = (x, y-1, 0, -1)
-            #print(f"{count} r1=> {ray}", flush=True)
             rays.append(ray1)
-            #count_energized(count, grid, ener, ray)
 
             ray2 = (x, y+1, 0, +1)
-            #print(f"{count} r2=> {ray}", flush=True)
-            #count_energized(count, grid, ener, ray)
             rays.append(ray2)
-
-            #print("=> ray1 ", ray1, " ray2 ", ray2)
-
         elif c == '-':
 
             ray1 = (x-1, y, -1, 0)
-            #print(f"{count} r1=> {ray}")
-            #count_energized(count,grid, ener, ray)
             rays.append(ray1)
 
             ray2 = (x+1, y, 1, 0)
-            #print(f"{count} r2=> {ray}")
-            #count_energized(count, grid, ener, ray)
             rays.append(ray2)
-            #print("=> ray1 ", ray1, " ray2 ", ray2)
 
         elif c == '/':
             new_dir = {(1,0): (0,-1), (-1,0): (0,1), (0,1): (-1,0),  (0,-1): (1,0)}
@@ -192,11 +152,6 @@ def count_energized(count, grid, ener, ray, ray_path, previous_paths):
             dx, dy = new_dir[key]
 
             ray1 = (x+dx, y+dy, dx, dy)
-            #print(f"{count} r=> {ray}")
-
-            #count_energized(count, grid, ener, ray)
-            #rays.append(ray)
-            #print("=> ray1 ", ray1)
 
         elif c == '\\':
             new_dir = {(1,0): (0,1), (-1,0): (0,-1), (0,1): (1,0),  (0,-1): (-1,0)}
@@ -205,26 +160,7 @@ def count_energized(count, grid, ener, ray, ray_path, previous_paths):
             dx, dy = new_dir[key]
 
             ray1 = (x+dx, y+dy, dx, dy)
-            #print(f"{count} r=> {ray}")
 
-            #count_energized(count, grid, ener,ray)
-            #rays.append(ray)
-            #print("=> ray1 ", ray1)
-
-        #print(f"{count}------------------------")
-        for iy, l in enumerate(grid):
-            for ix, c in enumerate(l):
-                printed = False
-                r = ray
-                if r:
-                    rx, ry, drx, dry = r
-                    if rx == ix and ry == iy:
-                        #print(' X ', end='')
-                        printed = True
-                if not printed:
-                    #print(f" {c} ", end='')
-                    pass
-            #print()
 
 start = time.time()
 part2()
