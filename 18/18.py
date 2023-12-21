f1b5cc58e252c1e0d90600e299b40d32d3ffa641
Henrik Berglund <henrik.berglund@proagile.se>
2023-12-21 22:33:03 +0100

    .

--- 18/18.py
+++ 18/18.py
@@ -10,14 +10,35 @@ def readfile( day):
 
 def part1():
     lines = readfile(18)
-
     instr = []
     for line in lines:
         s1 = line.split(" ")
         instr.append((s1[0], int(s1[1]), s1[2][1:-1]))
 
+    coords = [(0,0)]
+    x = 0
+    y = 0
+    dir = {'R': (1,0), 'U' : (0,-1), 'D': (0,1), 'L': (-1,0)}
     for i in instr:
-        print (i)
+        d, l, col = i
+        dx, dy = dir[d]
+        for j in range(l):
+            x += dx
+            y += dy
+            coords.append((x,y))
+
+    max_x =0
+    max_y = 0
+    for c in coords:
+        x,y = c
+        if x > max_x:
+            max_x = x
+        if y > max_y:
+            max_y = y
+
+    print(max_x, max_y)
+
+
 
     #for line in grid:
     #    print(line)
